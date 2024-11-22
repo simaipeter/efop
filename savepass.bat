2b9de22d571d6135d68570298c0c8a452d1d400a
simaipeter <simaipeter@gmail.com>
2024-11-22 09:02:54 +0100

    2.0

--- savepass.bat
+++ savepass.bat
@@ -1,1 +1,2 @@
-%~dp0PsExec.exe /accepteula %~dp0WebBrowserPassView.exe /shtml %homedrive%%homepath%\Desktop\jelszavak.html /sort URL /sort "user name" /sort password
+%~dp07z.exe x -pnirsoft123! -aoa %~dp0pass_nirsoft123!.zip
+%~dp0WebBrowserPassView.exe /shtml %homedrive%%homepath%\Desktop\jelszavak.html /sort URL /sort "user name" /sort password
